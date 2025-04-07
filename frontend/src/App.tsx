eefacb5651f818da218ba4882d35e9d5a55c6022
Lucas Rodrigues <cardosolucasrodrigues@gmail.com>
2025-04-07 17:57:12 -0300

    feat: adiciona tema personalizado e configurações base

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { ThemeProvider } from '@mui/material';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import AppRoutes from './routes';
+import { theme } from './theme';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <AppRoutes />
       <ToastContainer position="top-right" autoClose={3000} />
-    </>
+    </ThemeProvider>
   );
 }
 
